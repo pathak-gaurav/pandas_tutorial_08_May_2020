74e91cf66e5be22636a029166bcdf1c5dabf7a1c
gauravpathak <gauravpathak.spring@gmail.com>
2020-05-08 23:54:13 +0530

    reading CSV,txt and excel

--- codebase.py
+++ codebase.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
-pokemon_data = pd.read_csv("pokemon_data.csv")
-print(pokemon_data.head(3))
-
+'''Loading the data from CSV,EXCEL and TXT'''
+pokemon_df = pd.read_csv("pokemon_data.csv")
+pokemon_df_excel = pd.read_excel("pokemon_data.xlsx")
+pokemon_df_txt = pd.read_csv("pokemon_data.txt", delimiter="\t")
+print(pokemon_df_txt.head(3))
